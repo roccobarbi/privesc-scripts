ef12b3bfbd11a7583bb349778e01726be3b6550d
rocco barbini <roccobarbi@gmail.com>
2021-07-26 08:12:54 +0200

    Rewritten using os instead of subprocess

--- enum.py
+++ enum.py
@@ -1,30 +1,36 @@
-import subprocess
+import os
 
-executable_dirs = ["/bin", "/usr/bin", "usr/sbin"]
+executable_dirs = ["/bin", "/usr/bin", "/usr/sbin"]
 
 
 def get_executable_file_list():
     output = []
-    for dir in executable_dirs:
-        command = ["ls", "-l", dir]
-        out = subprocess.Popen(command, stdout=subprocess.PIPE)
-        stdout = out.communicate()
-        for line in stdout:
-            print(str(line))
-        output.extend(stdout)
+    for dir_name in executable_dirs:
+        file_list = os.listdir(dir_name)
+        for file_name in file_list:
+            file_path = os.path.join(dir_name, file_name)
+            output.append(file_path)
     return output
 
 
-def get_suid_and_sgid_from_list(list):
+def get_suid_from_list(file_list):
     output = []
-    for line in list:
-        if line[3] == 's' or line[6] == 's':
-            output.append(line)
+    for file_path in file_list:
+        if os.stat(file_path).st_mode & 0o4000:
+            output.append(file_path)
     return output
 
 
-def print_by_line(list):
-    for line in list:
+def get_sgid_from_list(file_list):
+    output = []
+    for file_path in file_list:
+        if os.stat(file_path).st_mode & 0o2000:
+            output.append(file_path)
+    return output
+
+
+def print_by_line(output):
+    for line in output:
         print(str(line))
 
 
@@ -40,9 +46,9 @@ def print_title(title):
 
 def main():
     executable_files = get_executable_file_list()
-    suid_sgid_executable_files = get_suid_and_sgid_from_list(executable_files)
-    print_title("Executable files with suid and guid")
-    print_by_line(suid_sgid_executable_files)
+    suid_executable_files = get_suid_from_list(executable_files)
+    print_title("Executable files with suid")
+    print_by_line(suid_executable_files)
 
 
 if __name__ == "__main__":
